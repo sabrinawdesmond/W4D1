6f8701878bea5a1baac588c77addb22876057a77
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-01-30 14:00:42 -0500

    with poly

--- knight_project.rb
+++ knight_project.rb
@@ -6,18 +6,24 @@ class KnightPathFinder
     end
 
     def self.valid_move(pos)
-        possible = [[2, 2], [2, 0], [1, 3], [2, 5], [2, 7], [1, 4]]
+      row, col = pos
+      if (row > - 1 && row < 8) && (col > - 1 && col < 8)
+        return true
+      end
+      false 
     end
 
-    def initialize
-        @size = 8 * 8
-        kpf = KnightPathFinder.new(0, 1)
+    def initialize(pos)
+        # @size = 8 * 8
+        # kpf = KnightPathFinder.new(0, 1)
         @grid = Array.new(8) { Array.new(8, ' ') }
         @parent = nil
         @children = []
+        self.root_node = pos
     end   
 
     def build_move_tree
         start = self.root_node(pos)
     end
 end
+
