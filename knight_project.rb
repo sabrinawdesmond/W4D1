7358a5e2834c8521431fa996ab94bfdba07d91a1
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-01-30 15:03:11 -0500

    start valid

--- knight_project.rb
+++ knight_project.rb
@@ -1,4 +1,4 @@
-class KnightPathFinder
+ class KnightPathFinder
     
     def self.root_node(pos)
         row, col = pos
@@ -7,7 +7,7 @@ class KnightPathFinder
 
     def self.valid_move(pos)
       row, col = pos
-      if (row > - 1 && row < 8) && (col > - 1 && col < 8)
+      if (row > -1 && row < 8) && (col > -1 && col < 8)
         return true
       end
       false 
@@ -19,11 +19,24 @@ class KnightPathFinder
         @grid = Array.new(8) { Array.new(8, ' ') }
         @parent = nil
         @children = []
+
         self.root_node = pos
     end   
 
-    def build_move_tree
-        start = self.root_node(pos)
+    def new_move_postions(pos)
+      old_pos = pos
+
     end
+
+    def build_move_tree
+        new_path = PolyTreeNode.new(self.root_node)
+        @parent = self.root_node
+        @considered_pos << @parent.children
+        # @considered_pos = @children.dup
+    end
+
+    # parents array that starts with self.root_node
+    # self.root_node.children >> children array
+    #  considered postitions array a
 end
 
